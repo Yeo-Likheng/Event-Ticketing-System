df466313ef0df0749cfac7bd293391bfe66ba1c3
Likheng Yeo <2023342yeo@aupp.edu.kh>
2026-05-13 11:53:13 -0500

    Switch to SendGrid SMTP for reliable email delivery in deployment

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,16 +2,15 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+    const transporter = nodemailer.createTransporter({
+      host: 'smtp.sendgrid.net',
       port: 587,
       secure: false,
       requireTLS: true,
-      family: 4,
 
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'apikey',
+        pass: process.env.SENDGRID_API_KEY,
       },
 
       connectionTimeout: 30000,
